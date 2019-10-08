71363d2d04c85beda8000092ada81a878e5bf219
viraj <virajshah2503@gmail.com>
2019-10-08 11:58:35 +0530

    Updates

---
+++ precache-manifest.0198b45e33f6e3fd9bfaf0869b2bf94e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d7f861b4e17b9b315632d481512675",
+    "url": "/swiggy-restaurants/index.html"
+  },
+  {
+    "revision": "c2769a84769ef47c4b18",
+    "url": "/swiggy-restaurants/static/css/main.84ddaaa8.chunk.css"
+  },
+  {
+    "revision": "0f51241ca5119b758b12",
+    "url": "/swiggy-restaurants/static/js/2.a33d30aa.chunk.js"
+  },
+  {
+    "revision": "c2769a84769ef47c4b18",
+    "url": "/swiggy-restaurants/static/js/main.6cfce094.chunk.js"
+  },
+  {
+    "revision": "e447891c53b1a2ff99bd",
+    "url": "/swiggy-restaurants/static/js/runtime-main.b35662d3.js"
+  }
+]);
