6bcb24105738c9409c58954edcff1953a5a093db
viraj <virajshah2503@gmail.com>
2019-10-08 12:00:46 +0530

    Updates

---
+++ precache-manifest.207dce00a33906d7fac4412aa881c89c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f959eb6e94b3995e828f694d3974d926",
+    "url": "/swiggy-restaurant-listing/index.html"
+  },
+  {
+    "revision": "87596b6d771a371889b1",
+    "url": "/swiggy-restaurant-listing/static/css/main.84ddaaa8.chunk.css"
+  },
+  {
+    "revision": "0f51241ca5119b758b12",
+    "url": "/swiggy-restaurant-listing/static/js/2.a33d30aa.chunk.js"
+  },
+  {
+    "revision": "87596b6d771a371889b1",
+    "url": "/swiggy-restaurant-listing/static/js/main.9006c563.chunk.js"
+  },
+  {
+    "revision": "efd796ecc016a697d77f",
+    "url": "/swiggy-restaurant-listing/static/js/runtime-main.62bcd036.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/swiggy-restaurants/precache-manifest.0198b45e33f6e3fd9bfaf0869b2bf94e.js"
+  "/swiggy-restaurant-listing/precache-manifest.207dce00a33906d7fac4412aa881c89c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/swiggy-restaurants/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/swiggy-restaurant-listing/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
